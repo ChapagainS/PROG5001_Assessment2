fb19fd94bf9f0302b908afe97394c0c4c21ab009
ChapagainS <165454800+ChapagainS@users.noreply.github.com>
2024-08-03 23:53:43 +1000

    EnterFile
    Taking Input from User

--- PROG5001_Assessment2/GradeFiles.java
+++ PROG5001_Assessment2/GradeFiles.java
@@ -10,13 +10,15 @@ import java.util.*;
 import java.io.File.*;
 import java.io.FileNotFoundException;
 
-public class ViewGrades
+public class GradeFiles
 {
     private static List<Students> students = new ArrayList<Students>();
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the name of the Student marking file: ");
-        
+        String fileName = scanner.nextLine();
+        readStudentData(fileName);
+        displayAllMarks();
     }
 }
